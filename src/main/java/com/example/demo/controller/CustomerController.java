9a1ef7c80ca0cecbddc55ef00566fd76dd9bb872
SwadDeeHawSudLoa <127951003+SwadDeeHawSudLoa@users.noreply.github.com>
2024-02-23 01:54:30 +0700

    new Post metode

--- src/main/java/com/example/demo/controller/CustomerController.java
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -3,11 +3,16 @@ package com.example.demo.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.hc.core5.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.Customer;
@@ -42,5 +47,12 @@ public class CustomerController {
 		return repository.findById(id) ;
 	}
 	
-
+	@PostMapping("/customer")
+    @ResponseBody
+    public List<Customer> postResponseJsonContent() {
+        return repository.findAll(); 
+    }
+   
 }
+
+
